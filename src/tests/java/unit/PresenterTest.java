8f00e040da04b5489066dc45cd52a7506bcd0075
Eirik Wiig <eirikwiig@gmail.com>
2016-04-17 13:53:53 +0100

    Remove whitespace in PresenterTest

--- src/tests/java/unit/PresenterTest.java
+++ src/tests/java/unit/PresenterTest.java
@@ -4,10 +4,8 @@ import main.java.Presenter;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
 import static org.junit.Assert.*;
 
 public class PresenterTest {
